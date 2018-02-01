9d305239c5260aeef724829163ed0b49be871c1c
Sander Saelmans <sander.saelmans@sogeti.com>
2018-02-01 23:01:15 +0100

    Added protocols to support

--- calabrother/ViewController.swift
+++ calabrother/ViewController.swift
@@ -6,19 +6,17 @@
 //
 
 import UIKit
+import ExternalAccessory
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, EAAccessoryDelegate {
+    
+    func accessoryDidDisconnect(_ accessory: EAAccessory) {
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func connectedAccessories() -> [EAAccessory] {
+        return EAAccessoryManager.shared().connectedAccessories
     }
-
 
 }
 
